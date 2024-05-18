08fce5e9e65b7dda6417bedb1a4e9864811ff243
VoiceShifter <gdy2003@yandex.ru>
2024-05-18 06:30:58 -0800

    init commit

---
+++ Logger.cpp
@@ -0,0 +1,19 @@
+
+#include "Logger.h"
+
+int main(unsigned int argc, char **argv)
+{
+      if (argc > 2)
+      {
+            std::cout << "Too many arguments\n";
+            return 0;
+      }
+      else
+      {
+            LoggerParser Parser(argv[1]);
+            Parser.AnalyzeLog();
+//          Parser.PrintTokensLine(1);
+
+      }
+      
+}
